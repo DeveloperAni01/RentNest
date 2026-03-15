31cc0ba3e1c30157e0dd68a04ae3368c8f46c856
developer_ani <developerani45@gmail.com>
2026-03-15 19:55:15 +0530

    testing project add

---
+++ Backend/RentNest/RentNest.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace RentNest.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
