bd8ca017de208db26d6d98e310b38186291516fc
developer_ani <developerani45@gmail.com>
2026-03-16 16:17:27 +0530

    new DB add and new migrations added successfully

---
+++ Backend/RentNest/RentNest.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace RentNest.Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=DESKTOP-ANIRBAN\\SQLEXPRESS;Database=RentNestDB;Trusted_Connection=True;TrustServerCertificate=True;",
+                sql => sql.MigrationsAssembly("RentNest.Infrastructure")
+            );
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
